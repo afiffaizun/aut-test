25003be08ed11d11f9e28cd3696277e3a63ad254
afiffaizun <afiffaizun31@gmail.com>
2026-04-26 08:09:29 +0700

    feat: add Redis connection and token blacklist service

---
+++ internal/infrastructure/redis/redis.go
@@ -0,0 +1,55 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Redis struct {
+	*redis.Client
+}
+
+func Connect(ctx context.Context, addr, password string, db int) (*Redis, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis: %w", err)
+	}
+
+	return &Redis{Client: client}, nil
+}
+
+func (r *Redis) Close() error {
+	return r.Close()
+}
+
+type BlacklistService struct {
+	redis *Redis
+}
+
+func NewBlacklistService(redis *Redis) *BlacklistService {
+	return &BlacklistService{redis: redis}
+}
+
+func (s *BlacklistService) AddToBlacklist(ctx context.Context, tokenID string, expiry time.Duration) error {
+	return s.redis.Set(ctx, "blacklist:"+tokenID, "1", expiry).Err()
+}
+
+func (s *BlacklistService) IsBlacklisted(ctx context.Context, tokenID string) (bool, error) {
+	result, err := s.redis.Exists(ctx, "blacklist:"+tokenID).Result()
+	if err != nil {
+		return false, err
+	}
+	return result > 0, nil
+}
+
+func (s *BlacklistService) RemoveFromBlacklist(ctx context.Context, tokenID string) error {
+	return s.redis.Del(ctx, "blacklist:"+tokenID).Err()
+}
